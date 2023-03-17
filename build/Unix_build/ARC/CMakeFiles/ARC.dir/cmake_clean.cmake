796ce2331fdadf7c1d2f9c106ffd71f6defeff90
ArterioRodrigues <arterio369@gmail.com>
2023-03-17 19:53:23 -0400

    Working build

---
+++ build/Unix_build/ARC/CMakeFiles/ARC.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ARC.dir/cmake_pch.hxx.pch"
+  "CMakeFiles/ARC.dir/cmake_pch.hxx.pch.d"
+  "CMakeFiles/ARC.dir/src/ArcApp.cpp.o"
+  "CMakeFiles/ARC.dir/src/ArcApp.cpp.o.d"
+  "CMakeFiles/ARC.dir/src/GLFWcode/GLFWimplementation.cpp.o"
+  "CMakeFiles/ARC.dir/src/GLFWcode/GLFWimplementation.cpp.o.d"
+  "CMakeFiles/ARC.dir/src/GameWindow.cpp.o"
+  "CMakeFiles/ARC.dir/src/GameWindow.cpp.o.d"
+  "libARC.dylib"
+  "libARC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ARC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Unix_build/S23_GAME/CMakeFiles/S23_Game.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "."
+  "../S23_Game.pdb"
+  "CMakeFiles/S23_Game.dir/src/main.cpp.o"
+  "CMakeFiles/S23_Game.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/S23_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
